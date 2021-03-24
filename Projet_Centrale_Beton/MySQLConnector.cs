4175bd8a9f728a5e98f031bda71a3709077ea73b
theo parent <theoparent71@gmail.com>
2021-03-24 08:44:03 +0100

    test commit

---
+++ Projet_Centrale_Beton/MySQLConnector.cs
@@ -0,0 +1,139 @@
+﻿/// TODO Discuter avec Benjamin de la forme de la BDD
+/// TODO Préparer la forme en fonction des rows de la BDD
+
+
+using System;
+using System.Data.SqlClient;
+using System.Net;
+using System.Data;
+using Google.Protobuf;
+using MySql.Data.MySqlClient;
+using Ubiety.Dns.Core;
+
+
+namespace Projet_Centrale_Beton
+{
+    public class MySQLConnector
+    {
+        private string ipServer;
+        private string database;
+        private string user;
+        private string password;
+        private string statement,statement2;
+        private string databaseString;
+        private MySqlConnection db;
+        private int index;
+        private object[] table;
+
+
+        /// <summary>
+        /// Création de l'objet MySQLConnector. Cet objet permet, au travers de ces méthodes, de se connecter à la BDD
+        /// uniquement en MySQL (uniquement en local pour le moment WIP).
+        /// </summary>
+        /// <param name="ipServer"></param>
+        /// <param name="database"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        public MySQLConnector(string ipServer, string database, string user, string password)
+        {
+            this.ipServer = ipServer;
+            this.database = database;
+            this.user = user;
+            this.password = password;
+            databaseString = "Server=" + ipServer + ";User ID=" + user + ";Password=" + password + ";Database=" +
+                             database;
+        }
+
+        public MySQLConnector()
+        {
+        }
+
+
+        /// <summary>
+        /// Connexion à la Base de données et premier test de connexion. Cette méthode, comme la déconnexion, ne peuvent être utilisés
+        /// que dans cette classe et ne peuvent être inhérité.
+        /// </summary>
+        private void Connect()
+        {
+            try
+            {
+                db = new MySqlConnection(databaseString);
+                db.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Déconnexion de la Base de Données. Cette méthode, comme la déconnexion, ne peuvent être utilisés
+        /// que dans cette classe et ne peuvent être inhérité.
+        /// </summary>
+        private void Disconnect()
+        {
+            db.Close();
+        }
+
+        public void PremierTest()
+        {
+            Connect();
+
+            Console.WriteLine("Première statement test :");
+            statement = "SELECT * FROM commandes";
+            MySqlCommand cmd = new MySqlCommand(statement, db);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            
+
+            int i = 0;
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0] + "" + reader[1] + " " + reader[2] + " " + reader[3]);
+                i++;
+            }
+
+
+            Disconnect();
+        }
+
+
+        public void CheckDriverUID(string id)
+        {
+            Connect();
+
+            statement = "SELECT * FROM encours WHERE UID =" + id;
+            MySqlCommand cmd = new MySqlCommand(statement, db);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Console.WriteLine("ID Correct");
+                index = reader[0].GetHashCode();
+                table = new[] {reader[0],reader[1],reader[2],reader[3],reader[4],reader[5]};
+            }
+            else
+            {
+                Console.WriteLine("Fake ID");
+            }
+            
+            TransfertFinishedOrder(table);
+            Disconnect();
+        }
+
+
+        private void TransfertFinishedOrder(object[] table)
+        {
+            Connect();
+
+            statement2 = "INSERT INTO conclus(UID,Nom,Prenom,Montant) VALUES ('" + table[2].ToString() + "','" + table[3].ToString() + "','" + table[4].ToString() + "','" + table[5].ToString() + "')";
+            MySqlCommand cmd = new MySqlCommand(statement2, db);
+            cmd.ExecuteNonQuery();
+            
+            Disconnect();
+        }
+        
+        
+    }
+}
---
+++ Projet_Centrale_Beton/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading;
+using Org.BouncyCastle.Asn1.X509;
+using Renci.SshNet;
+using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
+
+
+///TODO Discuter avec Foray de la forme du RaspBerry dans le système, avec un LCD, ou retentez NFC. 
+
+namespace Projet_Centrale_Beton
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Pi.Init<BootstrapWiringPi>();
+            
+            string id, database, password, ip, saisie;
+            int exit = 1;
+            RS232Controller controller = new RS232Controller();
+            IHM lcd = new IHM();
+            
+
+            ip = "10.0.0.111";
+            id = "root";
+            password = "";
+            database = "centraleabpe";
+            
+            MySQLConnector bddConnector = new MySQLConnector(ip, database, id, password);
+
+
+
+            string test = password + id;
+
+            while (exit != 0)
+            {
+                Console.WriteLine("1 : Scan code barres");
+                Console.WriteLine("2 : Vérification scan BDD d'un scan");
+                Console.WriteLine("4 : Test d'écriture sur LCD");
+                Console.WriteLine("Voici les ports séries à dispositions :");
+                controller.ListSerialPort();
+                
+
+                saisie = Console.ReadLine();
+                
+
+
+                switch (saisie)
+                {
+                    case "1":
+                        Console.WriteLine(controller.ReadSerialPort());
+                        break;
+
+                    case "2":
+                        Console.WriteLine("En attente d'un scan");
+                        bddConnector.CheckDriverUID(controller.ReadSerialPort());
+                        break;
+                    case "3":
+                        
+                        
+                        break;
+                    case "4":
+                        
+                        lcd.EcritureTest();
+                        break;
+
+
+                    case "0":
+                        exit = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("Erreur de saisie");
+                        break;
+                }
+            }
+        }
+    }
+}
