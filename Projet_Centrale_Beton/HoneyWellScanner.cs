4f80f681476012408db699cd06c291acdd387ecd
theo parent <theoparent71@gmail.com>
2021-03-24 08:42:13 +0100

    test commit

---
+++ Projet_Centrale_Beton/HoneyWellScanner.cs
@@ -0,0 +1,11 @@
+﻿namespace Projet_Centrale_Beton
+{
+    public class HoneyWellScanner : RS232Controller
+    {
+        public HoneyWellScanner()
+        {
+            RS232Controller sp = new RS232Controller("/dev/ttyUSB0");
+        }
+        
+    }
+}
---
+++ Projet_Centrale_Beton/RS232Controller.cs
@@ -0,0 +1,75 @@
+﻿using System.IO.Ports;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using Org.BouncyCastle.Utilities;
+
+namespace Projet_Centrale_Beton
+{
+    public class RS232Controller
+    {
+        private static SerialPort sp;
+
+
+        public RS232Controller()
+        {
+        }
+
+        public RS232Controller(string port)
+        {
+            sp = new SerialPort(port, 9600, Parity.None, 8,StopBits.Two);
+        }
+
+
+        /// <summary>
+        /// Ouverture du port RS232 sur le port USB0
+        /// </summary>
+        private void OpenConnector()
+        {
+            sp.Open();
+        }
+
+        /// <summary>
+        /// Fermeture du port RS232
+        /// </summary>
+        private void CloseConnector()
+        {
+            sp.Close();
+        }
+
+        
+        /// <summary>
+        /// Méthode pour lister tous les ports disponibles sur la machine Host
+        /// </summary>
+        public void ListSerialPort()
+        {
+            OpenConnector();
+            
+            int length = SerialPort.GetPortNames().Length;
+            for(int i = 0; i < length ; i++)
+            {
+                Console.WriteLine(SerialPort.GetPortNames()[i]);
+            }
+            
+            CloseConnector();
+        }
+        
+        /// <summary>
+        /// Retourne un string à l'arrivée d'une info dans le port série
+        /// </summary>
+        /// <returns></returns>
+        public string ReadSerialPort()
+        {
+            OpenConnector();
+            
+            string result = sp.ReadLine();
+            sp.DiscardInBuffer();
+
+            CloseConnector();
+            return result;
+        }
+        
+        
+        
+    }
+}
