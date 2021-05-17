09a387421baf57234cd64d5ee26d0f32fe5c9f4d
theo parent <theoparent71@gmail.com>
2021-05-17 14:58:58 +0200

    Déplacement du code fonctionnel dans CentraleController.cs

--- Projet_Centrale_Beton/Program.cs
+++ Projet_Centrale_Beton/Program.cs
@@ -13,47 +13,10 @@ namespace Projet_Centrale_Beton
         {
             
             Pi.Init<BootstrapWiringPi>();
-            
-            string path = "sqltest.json";
-            string JsonContent;
-            int exit = 1;
-            
-            
+
             CentraleController centrale = new CentraleController();
-
-
-            //Désérialisation JSON
-
-            JsonConfig config = new JsonConfig();
-            if (File.Exists(path))
-            {
-                JsonContent = File.ReadAllText(path);
-                config = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonConfig>(JsonContent);
-            }
-            else
-            {
-                config.Default();
-            }
+            centrale.Run();
             
-            MySQLConnector bddConnector = new MySQLConnector(config);
-            RS232Controller controller = new RS232Controller(config.sp_scanner);
-            IHM lcd = new IHM(config.sp_ihm);
-            
-            //Début programme de scan
-
-            while (exit != 0)
-            {
-
-                Console.WriteLine("En attente de scan...");
-                string result = controller.ReadSerialPort();
-                
-                if (bddConnector.CheckDriverUID(result))
-                {
-                    Console.WriteLine("Préparation de la commande...");
-                    centrale.FirstStage();
-                }
-            
-            }
         }
     }
 }
