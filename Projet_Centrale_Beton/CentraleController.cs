2e8782352571a681960b9ca6490bd44c3dd72ad8
theo parent <theoparent71@gmail.com>
2021-05-17 14:57:48 +0200

    Création du controle de la CentraleController.cs Mise en place de tout le fonctionnement du système

--- Projet_Centrale_Beton/CentraleController.cs
+++ Projet_Centrale_Beton/CentraleController.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
+using Phidget22;
 using Unosquare.RaspberryIO;
 using Unosquare.RaspberryIO.Abstractions;
 using Unosquare.WiringPi;
@@ -7,25 +10,81 @@ namespace Projet_Centrale_Beton
 {
     public class CentraleController
     {
+        private string result;
+        private RS232Controller controller;
+        private IHM lcd;
+        private MySQLConnector bddConnector;
+        
+
         public CentraleController()
         {
-            Pi.Init<BootstrapWiringPi>();
-            IHM ihm = new IHM();
+
+        }
+
+        public void Run()
+        {
+            string path = "sqltest.json";
+            string JsonContent;
+
+            CentraleController centrale = new CentraleController();
+            
+            //Désérialisation JSON
+
+            JsonConfig config = new JsonConfig();
+            if (File.Exists(path))
+            {
+                JsonContent = File.ReadAllText(path);
+                config = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonConfig>(JsonContent);
+            }
+            else
+            {
+                config.Default();
+            }
+            
+            //Construction des objets
+            
+            bddConnector = new MySQLConnector(config);
+            controller = new RS232Controller(config.sp_scanner);
+            lcd = new IHM(config.sp_ihm);
+            
+            int exit = 1;
+
+            if(exit != 0)
+            {
+                lcd.WriteWaitScan();
+                Console.WriteLine("attente de scan");
+                result = controller.ReadSerialPort();
+
+                if (bddConnector.CheckDriverUID(result))
+                {
+                    lcd.WriteStateScan("Production de la commande");
+                    FirstStage();
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Debut déplacement commande");
+                    FinishedOrder();
+                }
+                else
+                {
+                    lcd.WriteWaitScan();
+                }
+            }
+
+
 
         }
 
 
-        public void FirstStage()
+        private void FirstStage()
         {
-            var port = Pi.Gpio[BcmPin.Gpio16];
+            var port = Pi.Gpio[BcmPin.Gpio29];
             port.PinMode = GpioPinDriveMode.Output;
             port.Write(true);
-            
+
             Thread.Sleep(100);
 
         }
 
-        public void SecondStage()
+        private void SecondStage()
         {
 
             var port = Pi.Gpio[BcmPin.Gpio29];
@@ -36,7 +95,7 @@ namespace Projet_Centrale_Beton
 
         }
 
-        public void ThirdStage()
+        private void ThirdStage()
         {
 
             var port = Pi.Gpio[BcmPin.Gpio31];
@@ -45,6 +104,18 @@ namespace Projet_Centrale_Beton
             
             Thread.Sleep(100);
 
+        }
+
+        /// <summary>
+        /// Méthode indiquant la fin de la commande.
+        /// Ecriture de fin et pause 10s
+        /// </summary>
+        private void FinishedOrder()
+        {
+            lcd.WriteStateScan("Fin de la commande");
+            bddConnector.SwitchTables(result);
+            Thread.Sleep(1000);
+            
         }
     }
 }
